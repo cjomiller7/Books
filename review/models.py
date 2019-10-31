e0343a36d1af8c2426f3da9d79dca45eea5f9d78
cjomiller7 <miller.curtis@bls.gov>
2019-10-31 11:20:35 -0400

    second commit

---
+++ review/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+from django.conf import settings
+from django.utils import timezone
+
+class genre_key(models.Model):
+    key =
+
+
+TestModel.objects.get_or_create(testkey=TestModel2(id=1))
+
+
+class Rev(models.Model):  # class indicates we are defining an object, model.Models means object is a Django model
+    title = models.CharField(max_length=200)  # fields
+    author = models.CharField(max_length=200)
+    price = models.FloatField()
+    GENRE_CHOICE = (
+        ('Sci-FI', 'Sci-Fi'),
+        ('Mystery', 'Mystery'),
+        ('Children', 'Children'),
+        ('Horror', 'Horror'),
+        ('Political Thriller', 'Political Thriller'),
+        ('Self-Help', 'Self-Help'),
+        ('Biography', 'Biography'),
+        ('Textbook', 'Textbook'),
+        ('Picture Book', 'Picture Book'),
+        ('Travel', 'Travel'),
+    )
+    genre = models.CharField(max_length=50, choices=GENRE_CHOICE, null=True)
+    AGE_CHOICE = (
+        ('Toddler', 'Toddlers'),
+        ('Kids', 'Kids'),
+        ('Young Adult', 'Young Adult'),
+        ('College', 'College'),
+        ('Adults', 'Adults'),
+    )
+    age = models.CharField(max_length=50, choices=AGE_CHOICE, null=True)
+    review = models.TextField(max_length=2000)
+
+    def __str__(self):
+        return self.title
+
+
+
+# python manage.py makemigrations review
+# python manage.py migrate review
+
